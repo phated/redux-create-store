d3513a7c15658f5b5228bd91e4e8bfc106d73d73
Blaine Bublitz <blaine@iceddev.com>
2015-07-30 00:13:32 -0700

    initial impl

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var redux = require('redux');
+var promiseMiddleware = require('redux-promise');
+
+var defaultMiddleware = [promiseMiddleware];
+
+function createStore(reducers, extraMiddleware){
+  var reducer = redux.combineReducers(reducers);
+  var middleware = defaultMiddleware.concat(extraMiddleware);
+  var middlewareStack = redux.applyMiddleware.apply(null, middleware);
+  var storeCreator = middlewareStack(redux.createStore);
+  return storeCreator(reducer);
+}
+
+module.exports = createStore;
---
+++ test/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var lab = exports.lab = require('lab').script();
+var code = require('code');
+
+var createStore = require('../');
+
+lab.experiment('createStore', function(){
+
+  var reducers;
+  var middlewareCalls;
+
+  function testMiddleware(){
+    return function(next){
+      return function(action){
+        middlewareCalls++;
+        next(action);
+      };
+    };
+  }
+
+  lab.beforeEach(function(done){
+    middlewareCalls = 0;
+    reducers = {
+      test: function(state, action){
+        switch(action.type){
+          default:
+            return 1;
+        }
+      }
+    };
+    done();
+  });
+
+  lab.test('takes an object of reducers and returns a store', function(done){
+    var store = createStore(reducers);
+    code.expect(store.dispatch).to.be.a.function();
+    code.expect(store.getState).to.be.a.function();
+    var state = store.getState();
+    code.expect(state).to.contain({ test: 1 });
+    done();
+  });
+
+  lab.test('takes an array of middleware and returns a store', function(done){
+    var store = createStore(reducers, [testMiddleware]);
+    code.expect(store.dispatch).to.be.a.function();
+    code.expect(store.getState).to.be.a.function();
+    store.dispatch({ type: 'TEST' });
+    code.expect(middlewareCalls).to.equal(1);
+    done();
+  });
+
+  lab.test('takes a single middleware and returns a store', function(done){
+    var store = createStore(reducers, testMiddleware);
+    code.expect(store.dispatch).to.be.a.function();
+    code.expect(store.getState).to.be.a.function();
+    store.dispatch({ type: 'TEST' });
+    code.expect(middlewareCalls).to.equal(1);
+    done();
+  });
+});
